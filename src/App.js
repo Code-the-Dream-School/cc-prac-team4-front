397f1730bcfe8b66c7c15416d9f55715add213c0
OxiBo <xenos2000@ukr.net>
2023-05-18 22:15:03 -0400

    Add media query to manage base font-size

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { getAllData } from './util/index';
+import React, { useState, useEffect } from "react";
+import { getAllData } from "./util/index";
+import "./App.css";
 
-const URL = 'http://localhost:8000/api/v1/';
+const URL = "http://localhost:8000/api/v1/";
 
 function App() {
-
-const [message, setMessage] = useState(''); 
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-
     (async () => {
-      const myData = await getAllData(URL)
+      const myData = await getAllData(URL);
       setMessage(myData.data);
     })();
-      
-    return () => {
-      console.log('unmounting');
-    }
 
+    return () => {
+      console.log("unmounting");
+    };
   }, []);
 
   return (
     <>
       <h1>{message}</h1>
+      <main>
+        <div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic,
+            explicabo!
+          </p>
+        </div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
+          reiciendis architecto vitae neque placeat maxime repudiandae inventore
+          voluptates, sit nihil?
+        </p>
+        SOme stuff
+      </main>
     </>
   );
 }
